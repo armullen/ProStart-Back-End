e3054f44084a9b2ee9b71ea8c205fea967928eda
armullen <amber.r.mullen@gmail.com>
2023-07-11 19:23:16 -0500

    camp model added

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Camp
 
 # Register your models here.
+
+class CampAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'ageGroup', 'startDate', 'endDate', 'time', 'price')
+
+admin.site.register(Camp, CampAdmin)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Camp(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    ageGroup = models.CharField(max_length=500)
+    startDate = models.DateField(max_length=200)
+    endDate = models.DateField(max_length=200)
+    time = models.TimeField(max_length=200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
